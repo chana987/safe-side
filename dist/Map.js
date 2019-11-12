fe282701bbc0d6bef170baeed19e0b71d9f45e2b
Guy <guydaane@gmail.com>
2019-11-12 17:10:16 +0200

    added marker successfully

--- dist/Map.js
+++ dist/Map.js
@@ -1,19 +1,26 @@
 function initialize() {
     var mapOptions = {
-        center: new google.maps.LatLng(32.0853, 34.7818),
-        zoom: 9,
+        center: new google.maps.LatLng(32.060033, 34.769145),
+        zoom: 14,
         mapTypeId: google.maps.MapTypeId.HYBRID,
-        scrollwheel: false,
-        draggable: false,
+        scrollwheel: true,
+        draggable: true,
         panControl: true,
         zoomControl: true,
         mapTypeControl: false,
-        scaleControl: false,
+        scaleControl: true,
         streetViewControl: false,
         overviewMapControl: true,
         rotateControl: false
-    };
+    }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var testReviews = {lat: 32.063032, lng: 34.774198}
+  
+    var marker = new google.maps.Marker({
+        position: testReviews,
+        map: map,
+    })
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
