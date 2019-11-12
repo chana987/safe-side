1d31388ba95bcd586c1d19b6eb581421c455339f
Guy <guydaane@gmail.com>
2019-11-12 15:49:44 +0200

    map is workingish

--- dist/Map.js
+++ dist/Map.js
@@ -1,30 +1,30 @@
-class Map {
-    initialize() {
-        const mapOptions = {
-            center: new google.maps.LatLang(32.0853, 34.7818),
-            zoom: 10,
-            mapTypeId: google.maps.MapTypeId.HYBRID,
-            scrollwheel: false,
-            draggable: false,
-            panControl: true,
-            zoomControl: true,
-            mapTypeControl: false,
-            scaleControl: false,
-            overviewMapControl: true,
-            rotateControl: false
-        }
-        let map = new google.maps.Map(document.getElementById("map"), mapOptions)
-        google.maps.event.addDomListener(window, 'load', initialize)
-    }
-
-    showReviews() {
-
-    }
-
-    mapOptions() {
-
-    }
-    getLayers() {
-
-    }
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(32.0853, 34.7818),
+        zoom: 9,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        scrollwheel: false,
+        draggable: false,
+        panControl: true,
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        overviewMapControl: true,
+        rotateControl: false
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    // showReviews() {
+
+    // }
+
+    // mapOptions() {
+
+    // }
+    // getLayers() {
+
+    // }
+
