0068d06522863b009304b32229508dde6315754d
Chana <chana987@gmail.com>
2019-11-12 15:21:00 +0200

    formatting and typos

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -1,7 +1,5 @@
 class Renderer {
-
     renderData(review) {
-
         const source = $("#review-template").html()
         const template = Handlebars.compile(source)
         let newHTML = template({review})
