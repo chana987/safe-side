ccef02f02e2ffa4a3afbf5e9dbe154f921384408
ori <oritsadok1@gmail.com>
2019-11-13 23:56:01 +0200

    addd

--- dist/Map.js
+++ dist/Map.js
@@ -3,6 +3,8 @@ let newMarker
 let geocoder
 let mapOptions
 let map
+let infowindow 
+let marker
 
 function initialize() {
 
@@ -60,11 +62,11 @@ function initialize() {
     const makeMarker = (time, people, cleanliness, lighting, content, lat, lng) => {
         let contentString = `<div class="review-info-content"><p class="review-info-time">Time: ${time}</p><p class="review-info-people">Crowds: ${people}</p><p class="review-info-cleanliness">Cleanliness: ${cleanliness}</p><p class="review-info-lighting">Lighting: ${lighting}</p><p class="review-info-content">Content: ${content}</p></div>`
 
-        let infowindow = new google.maps.InfoWindow({
+         infowindow = new google.maps.InfoWindow({
             content: contentString
         })
 
-        let marker = new google.maps.Marker({
+         marker = new google.maps.Marker({
             title: "review",
             map: map,
             position: { lat: lat, lng: lng }
@@ -103,13 +105,13 @@ function initialize() {
 
         var btn = document.getElementById('map-btn')
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(btn)
-        //makeReviewMarkers()
+        makeReviewMarkers()
     }
     function codeAddress(address) {
         geocoder.geocode({ address: address }, function (results, status) {
             if (status == "OK") {
                 map.setCenter(results[0].geometry.location)
-                const marker = new google.maps.Marker({
+                 marker = new google.maps.Marker({
                     map: map,
                     position: results[0].geometry.location
                 })
@@ -119,12 +121,51 @@ function initialize() {
             }
         })
     }
-
+    
     $(".search-street").on("click", () => {
         let address = document.getElementById("search-street-input").value
         codeAddress(address)
     })
 
+    function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: 40.731, lng: -73.997}
+  });
+  var geocoder = new google.maps.Geocoder;
+  var infowindow = new google.maps.InfoWindow;
+
+  document.getElementById('find-loc').addEventListener('click', function() {
+    geocodeLatLng(geocoder, map, infowindow);
+  });
 }
+
+function geocodeLatLng(geocoder, map, infowindow) {
+  var latlngStr = input.split(',', 2);
+  var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+  geocoder.geocode({'location': latlng}, function(results, status) {
+      console.log(latlng)
+    if (status === 'OK') {
+      if (results[0]) {
+        map.setZoom(15);
+         marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+        infowindow.setContent(results[0].formatted_address);
+        infowindow.open(map, marker);
+       // console.log(infowindow)
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+}
+}
+
+
+
 
 google.maps.event.addDomListener(window, "load", initialize)
